0ad86648b161aab2b1c23dd7dedeed0156ab6bc9
xarunofin <jjsantos@finalehealth.com>
2024-08-27 07:29:03 +0800

    FEAT: mock 404 page

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './pages/todo/todo.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'todo', component: TodoComponent },
   { path: '', redirectTo: '/todo', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
---
+++ apps/web/src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  host: {
+    class: 'flex-1 flex flex-col items-center',
+  },
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found.component.html',
+})
+export class NotFoundComponent {}
